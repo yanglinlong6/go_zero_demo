8f659013e45fd658392584d221b00fd4409e6f46
杨林龙 <yangll@didihu.com.cn>
2023-11-03 16:28:36 +0800

    优化项目 添加测试用例

--- api/api_demo/internal/logic/apidemologic_test.go
+++ api/api_demo/internal/logic/apidemologic_test.go
@@ -31,11 +31,27 @@ func TestApi_demoLogic_Api_demo(t *testing.T) {
 		return
 	}
 	//得到返回结果
-	var result map[string]interface{}
+	var result Gen
 	body, err := ioutil.ReadAll(resp.Body)
 	if err == nil {
 		err = json.Unmarshal(body, &result)
 	}
 
 	fmt.Println(result)
+}
+
+type Gen struct {
+	UpdateTime string `json:"update_time" gorm:"column:update_time"`
+	Data       []struct {
+		MobilUrl string `json:"mobilUrl" gorm:"column:mobilUrl"`
+		Index    int    `json:"index" gorm:"column:index"`
+		Pic      string `json:"pic" gorm:"column:pic"`
+		Title    string `json:"title" gorm:"column:title"`
+		Hot      string `json:"hot" gorm:"column:hot"`
+		Url      string `json:"url" gorm:"column:url"`
+		Desc     string `json:"desc" gorm:"column:desc"`
+	} `json:"data" gorm:"column:data"`
+	Success  bool   `json:"success" gorm:"column:success"`
+	Subtitle string `json:"subtitle" gorm:"column:subtitle"`
+	Title    string `json:"title" gorm:"column:title"`
 }
